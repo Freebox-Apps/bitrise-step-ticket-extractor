a672c5c74c019f18c3109ad6f6039da204e0e904
Thibault Fighiera <tfighiera@freebox.fr>
2025-07-02 11:15:36 +0200

    chore(Clean): remove useless code and improved doc

--- main.go
+++ main.go
@@ -24,27 +24,12 @@ func main() {
 	}
 
 	ticketListStrigns := strings.Join(allTickets[:], getOutputDelimiter())
-	//entries := createEntries(prefixStrList)
-	//fillCommitInfo(commitStrList, entries)
 
-	//
-	// --- Step Outputs: Export Environment Variables for other Steps:
-	// You can export Environment Variables for other Steps with
-	//  envman, which is automatically installed by `bitrise setup`.
-	// A very simple example:
 	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "TICKET_LIST", "--value", ticketListStrigns).CombinedOutput()
 	if err != nil {
 		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
 		os.Exit(1)
 	}
-	// You can find more usage examples on envman's GitHub page
-	//  at: https://github.com/bitrise-io/envman
-
-	//
-	// --- Exit codes:
-	// The exit code of your Step is very important. If you return
-	//  with a 0 exit code `bitrise` will register your Step as "successful".
-	// Any non zero exit code will be registered as "failed" by `bitrise`.
 	os.Exit(0)
 }
 
